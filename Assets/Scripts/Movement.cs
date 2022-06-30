63fc308c1025ce1e134545e64ff403624658c7f9
jaysonh <mail@jaysonh.com>
2022-06-30 16:47:45 +0100

    added new files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+    private Camera cam;
+    public float MovementSpeed = 40.0f;
+    public float Gravity = 0.0f;
+    private float velocity = 0;
+    private const float MIN_X = -860.5f;
+    private const float MAX_X =  35.0f;
+    private const float MIN_Y =  30.0f;
+    private const float MAX_Y =  90.0f;
+    private const float MIN_Z = -3500.0f;
+    private const float MAX_Z = -3215.0f;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // player movement - forward, backward, left, right
+        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
+        // characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+        characterController.Move((cam.transform.right * horizontal + cam.transform.forward * vertical) * Time.deltaTime);
+
+        // Gravity
+        if (characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity -= Gravity * Time.deltaTime;
+            characterController.Move(new Vector3(0, velocity, 0));
+        }
+
+        // Clamp movement to inside building
+        transform.position = new Vector3(
+              Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
+              Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
+              Mathf.Clamp(transform.position.z, MIN_Z, MAX_Z));
+    }
+}
---
+++ Assets/Scripts/OSCHandler.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+
+
+public class OSCHandler : MonoBehaviour
+{
+
+    class DmxUsbDevice
+    {
+        public DmxUsbDevice(int _id, int _numChannels)
+        {
+            id = _id;
+            numChannels = _numChannels;
+
+            dmxVals = new int[numChannels];
+        }
+
+        public void update(int[] _dmxVals)
+        {
+            dmxVals = _dmxVals;
+        }
+
+        public int numChannels;
+
+        public int id;
+        public int[] dmxVals;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting OSC Handler");
+
+        var receiver = gameObject.AddComponent<OSCReceiver>();
+
+        // Set local port.
+        receiver.LocalPort = 7001;
+        receiver.Bind("/DMXData", ReceivedDMXData);
+        //receiver.Bind("/DMXInit", ReceivedDMXInit);
+        Debug.Log("Connected to OSC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void ReceivedDMXInit(OSCMessage message)
+    {
+        int deviceID    = message.Values[0].IntValue; // first value is device id
+        int numChannels = message.Values[1].IntValue; // second value is num dmx channels
+        
+        DmxUsbDevice d = new DmxUsbDevice(deviceID, numChannels);
+        deviceList.Add(d);
+
+        Debug.Log("Init device: " + deviceID);
+    }
+
+    protected void ReceivedDMXData(OSCMessage message)
+    {
+        bool foundDevice = false;
+        int  deviceID    = message.Values[0].IntValue; // First value is the device id
+
+        //Debug.Log("recv msg for device: " + deviceID + " numVals: " + numVals);
+        for ( int i = 0; i < deviceList.Count; i++ )
+        {
+            if(((DmxUsbDevice)deviceList[i]).id == deviceID )
+            {
+                foundDevice = true;
+
+                setDmxData(i, message);
+            }
+        }    
+
+        // If we couldn't find the device then add it to the list
+        if(!foundDevice)
+        {
+            Debug.Log("couldn't find device: " + deviceID + ", adding it");
+            int numChannels = message.Values.Count - 1;
+
+            DmxUsbDevice d = new DmxUsbDevice(deviceID, numChannels);
+            deviceList.Add(d);
+        }
+        
+    }
+
+    public void setDmxData(int deviceIndx, OSCMessage message)
+    {
+        //Debug.Log("setting dmx data for device: " + deviceIndx);
+        //int deviceID = message.Values[0].IntValue; // First value is the device id
+        int numChannels = message.Values.Count - 1;
+
+        int[] dmxVals = new int[numChannels];
+
+        for (int indx = 1; indx < numChannels; indx++)
+        {
+            dmxVals[indx] = message.Values[indx].IntValue;
+            //Debug.Log(dmxVals[indx - 1])
+        }
+
+        ((DmxUsbDevice)deviceList[deviceIndx]).update(dmxVals);
+    }
+    public int getValue(int deviceID, int channel)
+    {
+        int res = -1;
+
+        for (int i = 0; i < deviceList.Count; i++)
+        {
+            if (((DmxUsbDevice)deviceList[i]).id == deviceID)
+            {
+                res = ((DmxUsbDevice)deviceList[i]).dmxVals[channel];
+            }
+        }
+
+        return res;
+    }
+
+    private ArrayList deviceList = new ArrayList();
+
+}
